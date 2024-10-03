8d9523eb7cd736ffa46d7f44c9c910cb90107e0c
Aizenss <irsyaderpl1@gmail.com>
2024-10-04 00:55:41 +0700

    insyallah done

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const navLinks = document.getElementById('nav-links');
 navToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+
+const navLinksLiA = document.querySelectorAll('.nav-links li a');
+const sections = document.querySelectorAll('section');
+
+function changeActiveLink() {
+    let index = sections.length;
+
+    while (--index && window.scrollY + 60 < sections[index].offsetTop) {
+    }
+
+    navLinksLiA.forEach((link) => link.classList.remove('active'));
+    navLinksLiA[index].classList.add('active');
+}
+
+window.addEventListener('scroll', changeActiveLink);
